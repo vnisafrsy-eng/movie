29fa182df43cd7e28dfe8bbb9b55f16236c85ae4
vani <vnisafrsy@gmail.com>
2026-04-16 16:56:03 +0700

    memanggil class store pake use App diatas

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie;
 use App\Models\Category;
+use App\Http\Requests\StoreMovieRequest;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
