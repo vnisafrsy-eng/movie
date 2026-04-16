ddd313028ef18b0040642175d51f5cef647a496c
vani <vnisafrsy@gmail.com>
2026-04-16 12:44:16 +0700

    isi file stroreRequest dan movie controller

--- app/Http/Requests/StoreMovieRequest.php
+++ app/Http/Requests/StoreMovieRequest.php
@@ -3,26 +3,25 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreMovieRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'id' => ['required', 'string', 'max:255', Rule::unique('movies', 'id')],
+            'judul' => 'required|string|max:255',
+            'category_id' => 'required|integer|exists:categories,id',
+            'sinopsis' => 'required|string',
+            'tahun' => 'required|integer',
+            'pemain' => 'required|string',
+            'foto_sampul' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ];
     }
 }
