5f4d90d66ad2e577352598e513ec86f4f8b4a1b3
vani <vnisafrsy@gmail.com>
2026-04-29 14:47:45 +0700

    update bebrapa file dan penambahan beberapa folder baru pada app

--- app/Http/Requests/StoreMovieRequest.php
+++ app/Http/Requests/StoreMovieRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class StoreMovieRequest extends FormRequest
 {
@@ -15,12 +14,11 @@ class StoreMovieRequest extends FormRequest
     public function rules()
     {
         return [
-            'id' => ['required', 'string', 'max:255', Rule::unique('movies', 'id')],
-            'judul' => 'required|string|max:255',
+            'judul'       => 'required|string|max:255',
             'category_id' => 'required|integer|exists:categories,id',
-            'sinopsis' => 'required|string',
-            'tahun' => 'required|integer',
-            'pemain' => 'required|string',
+            'sinopsis'    => 'required|string',
+            'tahun'       => 'required|integer|min:1900|max:2030',
+            'pemain'      => 'required|string|max:255',
             'foto_sampul' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ];
     }
